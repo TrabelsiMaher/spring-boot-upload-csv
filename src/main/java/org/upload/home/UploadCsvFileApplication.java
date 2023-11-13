958591fbff1c61328ee2625ed673f4a07791bc17
maher <mahtrabelsi@gmail.com>
2023-11-13 18:52:41 +0100

    creation du projet

---
+++ src/main/java/org/upload/home/UploadCsvFileApplication.java
@@ -0,0 +1,13 @@
+package org.upload.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadCsvFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadCsvFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/upload/home/UploadCsvFileApplicationTests.java
@@ -0,0 +1,13 @@
+package org.upload.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadCsvFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
